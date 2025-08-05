6cd4f2e4242494fa68f55639c8011a1e26584bb2
imNaNye <hyunkyungj0730@gmail.com>
2025-08-05 18:18:57 +0900

    [add] add endPage components

---
+++ src/components/user/end/endmessage.jsx
@@ -0,0 +1,5 @@
+import './end.css';
+
+export default function EndMessage(){
+    return <h3 className="end-message">교육 이수가 종료되었습니다.</h3>;
+}
--- src/pages/UserPage/EndPage.jsx
+++ src/pages/UserPage/EndPage.jsx
@@ -1,3 +1,8 @@
+import EndMessage from "../../components/user/end/endmessage";
+import '../../components/user/end/end.css';
+
 export default function EndPage(){
-    return <div>EndPage</div>;
+    return <div classname="end-page">
+        <EndMessage></EndMessage>
+        </div>;
 }

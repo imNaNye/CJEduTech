dd89932efb5e18d514a1d3e62c991c189aed4943
imNaNye <hyunkyungj0730@gmail.com>
2025-08-07 14:31:26 +0900

    [chore] change NextSessionButton Link

--- src/components/user/discussionResult/NextSessionButton.jsx
+++ src/components/user/discussionResult/NextSessionButton.jsx
@@ -4,7 +4,7 @@ export default function NextSessionButton() {
   const navigate = useNavigate();
 
   return (
-    <button className="next-session-button" onClick={() => navigate("/")}>
+    <button className="next-session-button" onClick={() => navigate("/user/end")}>
       종료하기\(다음세션\)
     </button>
   );

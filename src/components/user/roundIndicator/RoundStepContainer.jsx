89fb8dcc15997494b18542d1e1b302c198cf21ad
imNaNye <hyunkyungj0730@gmail.com>
2025-08-28 22:13:33 +0900

    [merge]

--- src/components/user/roundIndicator/RoundStepContainer.jsx
+++ src/components/user/roundIndicator/RoundStepContainer.jsx
@@ -7,15 +7,15 @@ export default function RoundStepContainer({whichStep = 1}){
         <div className = "round-step-container">
             <div className = {((step === 1) ? 'current-step' : 'step-indicator')}>
                 <h4>step 1</h4>
-                <text>퀴즈 풀이</text>
+                퀴즈 풀이
             </div>
             <div className = {((step === 2) ? 'current-step' : 'step-indicator')}>
                 <h4>step 2</h4>
-                <text>영상 시청</text>
+                영상 시청
             </div>
             <div className = {((step === 3) ? 'current-step' : 'step-indicator')}>
                 <h4>step 3</h4>
-                <text>AI 토론</text>
+                AI 토론
             </div>
         </div>
     );

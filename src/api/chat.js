de54173340a9f270325bbac273ef01393b5d750f
Dripmaster <son342@gmail.com>
2025-09-05 13:42:25 +0900

    change back

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
-
+const API = import.meta.env.VITE_API_URL;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_URL;
 // 서버 주소에 맞게 수정
-export const socket = io("http://localhost:3000/chat", {
-  transports: ["websocket"]
+export const socket = io(SOCKET_URL+"/chat", {
+  transports: ["websocket"],
+  withCredentials: true,
 });

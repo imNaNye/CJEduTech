2644336abb58421f60e4ce63bf0bf70184625422
imNaNye <hyunkyungj0730@gmail.com>
2025-10-11 02:38:06 +0900

    [chore] change videos

--- src/contents/videoByRound.js
+++ src/contents/videoByRound.js
@@ -1,13 +1,14 @@
 // src/contents/videoByRound.js
 import r1_1 from '/videos/round1_1.mp4';
 import r1_2 from '/videos/round1_2.mp4';
-import r1_3 from '/videos/round1_3.mp4';
-import r2_1 from '/videos/round2_1.mp4';
-import r2_2 from '/videos/round2_2.mp4';
+import r1_3 from '/videos/round1_3.mov';
+import r2_1 from '/videos/round2_1.mov';
+import r2_2 from '/videos/round2_2.mov';
 import r2_3 from '/videos/round2_3.mp4';
-import r3_1 from '/videos/round3_1.mp4';
-import r3_2 from '/videos/round3_2.mp4';
-import r3_3 from '/videos/round3_3.mp4';
+import r3_1 from '/videos/round3_1.mov';
+import r3_2 from '/videos/round3_2.mov';
+import r3_3 from '/videos/round3_3.mov';
+import r3_4 from '/videos/round3_4.mov';
 
 export const videoByRound = {
   1: [
@@ -19,8 +20,8 @@ export const videoByRound = {
     { title: 'ep6 - SNS 대기불만폭주! 고객경험혁신', src: r2_3 },
     { title: 'ep7 - 시그니처 폭립의 화룡점정', src: r3_1 },
     { title: 'ep8 - VIPS 주방 내 갈등해결', src: r3_2 },
-    { title: 'ep10 - VIPS 주방 막내의 스마트한 식자재관리', src: r3_3 },
-    { title: '임시 동영상', src: r3_2 },
+    { title: 'ep9 - VIPS 칼 같은 계량의 중요성', src: r3_3 },
+    { title: 'ep10 - VIPS 주방 막내의 스마트한 식자재관리', src: r3_4 },
   ],
   2: [
     { title: 'Round 2 - 예약 시스템 오류와 고객의 분노', src: r2_1 },

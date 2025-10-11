019cec1154734ae95c505f2e2b7e2c3f35e580d9
imNaNye <hyunkyungj0730@gmail.com>
2025-10-11 23:33:32 +0900

    [chore] change onboarding text

--- src/components/user/onboarding/OnboardingImgText.jsx
+++ src/components/user/onboarding/OnboardingImgText.jsx
@@ -5,7 +5,7 @@ export default function OnboardingImgText(){
     return(
         <div className="onboarding-img-text">
             <img className="logo-robot" src={logoRobot}></img>
-            <OnboardingText heading="오늘 교육은?" p={"오늘 교육은 대한민국 미래세대인 아동 청소년의\n건강한 성장을 지원하기 위해 CJ그룹의 나눔재단에서 설립,\n운영하는 사회공헌플랫폼을 활용해 이수할 예정입니다."}></OnboardingText>
+            <OnboardingText heading="오늘 교육은?" p={"오늘 교육은 CJ도너스캠프 아카데미에서 준비한 \n다양한 시청각 교육 자료들을 바탕으로, \nCJ人 으로써의 기본 소양 및 직무 지식습득을 위한 \n여러 미디어리터러시 활동들을 수행해 볼 예정입니다  "}></OnboardingText>
         </div>
     )
 }
